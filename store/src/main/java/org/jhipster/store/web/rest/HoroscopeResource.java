7688f5d26df9eb99ec26b2e7e34c15c917815db1
alexandru.ciocan <alexandru.ciocan@vivacredit.ro>
2018-01-16 14:46:35 +0200

    Call to webservice from microservice - response as JSON.

--- store/src/main/java/org/jhipster/store/web/rest/HoroscopeResource.java
+++ store/src/main/java/org/jhipster/store/web/rest/HoroscopeResource.java
@@ -3,21 +3,16 @@ package org.jhipster.store.web.rest;
 /**
  * Created by alexandru.ciocan on 08/01/2018.
  */
-import com.codahale.metrics.annotation.Timed;
+
 import org.json.JSONException;
-import org.json.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
-import java.util.Map;
 
 /**
  * REST controller for managing Horoscope.
@@ -26,31 +21,27 @@ import java.util.Map;
 @RequestMapping("/api")
 public class HoroscopeResource {
 
-    private final Logger log = LoggerFactory.getLogger(ProductResource.class);
     private final String ipServiceUrl = "http://api.ipify.org?format=json";
 
-    @GetMapping(value = "/horoscope", produces = MediaType.TEXT_PLAIN_VALUE)
-    public ResponseEntity<String> horoscope() throws JSONException {
+    @GetMapping(value = "/horoscope", produces = "application/json")
+    public ResponseEntity<HashMap> horoscope() throws JSONException {
 
         RestTemplate restTemplate = new RestTemplate();
 
-        String response;
-        Map<String, String> ipData = new HashMap<>();
+        HashMap<String, String> response = new HashMap<>();
 
         try {
 
-            ipData = restTemplate.getForObject(ipServiceUrl, HashMap.class);
-            log.debug("Ip received:" + ipData.toString());
-            response =  ipData.get("ip");
+            response = restTemplate.getForObject(ipServiceUrl, HashMap.class);
 
-            return new ResponseEntity<String>(response, HttpStatus.OK);
-
+            return new ResponseEntity<HashMap>(response, HttpStatus.OK);
 
         } catch (Exception e) {
 
-            response = "exception " + e.getMessage();
+            response.put("exception", "true");
+            response.put("message", e.getMessage());
 
-            return new ResponseEntity<String>(response, HttpStatus.OK);
+            return new ResponseEntity<HashMap>(response, HttpStatus.OK);
 
         }
     }
